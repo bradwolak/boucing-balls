12fc6a67eea5277f222cabcf137a073d838b68b6
Brad <bradleywolak@gmail.com>
2019-01-29 11:23:45 -0500

    update main.js

--- main.js
+++ main.js
@@ -9,6 +9,6 @@ var height = canvas.height = window.innerHeight;
 // function to generate random number
 
 function random(min,max) {
-  var num = Math.floor(Math.random()*(max-min)) + min;
+  var num = Math.floor(Math.random()*(max-min + 1)) + min;
   return num;
 }
